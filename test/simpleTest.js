97d5ecf04b58dbf153eccb61f8b02c168a5029e8
tkdmatze <tkdmatze+github@googlemail.com>
2017-04-21 00:13:57 +0200

    added the first test

---
+++ test/simpleTest.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+
+describe('mocha works', function () {
+  it('should always be true if mocha works', function (done) {
+    assert.equal(true, 1 === 1);
+    done();
+  });
+});
